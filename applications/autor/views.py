3cd09706e37ebcee2d3986f0b8a0101021c0820c
Ezequiel Flores <supereze.bxb@gmail.com>
2020-05-07 14:53:40 -0500

    [feat] all autor view

--- applications/autor/views.py
+++ applications/autor/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Autor
 
-# Create your views here.
+
+class ListAutores(ListView):
+    template_name = "autor/lista.html"
+    context_object_name = "lista_autores"
+
+    def get_queryset(self):
+        return Autor.objects.all()
