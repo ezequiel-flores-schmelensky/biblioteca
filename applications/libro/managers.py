e0bd4902cb52c2712a5f72e7afd3c544c133e364
Ezequiel Flores <supereze.bxb@gmail.com>
2020-05-08 16:06:51 -0500

    [feat] related models and distinct

--- applications/libro/managers.py
+++ applications/libro/managers.py
@@ -4,14 +4,12 @@ from django.db.models import Q
 
 class LibroManager(models.Manager):
     
-    
     def listar_libros(self, kword):
         resultado = self.filter(
             titulo__icontains=kword,
             fecha__range=('2000-01-01', '2017-01-02')
         )
         return resultado
-
 
     def listar_libros2(self, kword, fecha1, fecha2):
 
@@ -24,6 +22,13 @@ class LibroManager(models.Manager):
         )
         return resultado
 
-
     def listar_libros_categoria(self, categoria):
         return self.filter(categoria__id=categoria).order_by('titulo')
+
+
+class CategoriaManager(models.Manager):
+
+    def categoria_por_autor(self, autor):
+        return self.filter(
+            categoria_libro__autores__id=autor
+        ).distinct()
--- applications/libro/models.py
+++ applications/libro/models.py
@@ -1,15 +1,21 @@
 from django.db import models
 from applications.autor.models import Autor
-from .managers import LibroManager
+from .managers import LibroManager, CategoriaManager
 # Create your models here.
 class Categoria(models.Model):
     nombre = models.CharField(max_length=30)
+
+    objects = CategoriaManager()
 
     def __str__(self):
         return str(self.id) + " - " + self.nombre
 
 class Libro(models.Model):
-    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(
+        Categoria, 
+        on_delete=models.CASCADE,
+        related_name="categoria_libro"
+    )
     autores = models.ManyToManyField(Autor)
     titulo = models.CharField(max_length=50)
     fecha = models.DateField('Fecha de lanzamiento')
