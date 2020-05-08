f7a1f622b7f9998f977e46a4922af0e24db82c00
Ezequiel Flores <supereze.bxb@gmail.com>
2020-05-08 12:52:42 -0500

    [feat] data range finished

--- applications/libro/managers.py
+++ applications/libro/managers.py
@@ -1,3 +1,4 @@
+import datetime
 from django.db import models
 from django.db.models import Q
 
@@ -8,5 +9,17 @@ class LibroManager(models.Manager):
         resultado = self.filter(
             titulo__icontains=kword,
             fecha__range=('2000-01-01', '2017-01-02')
+        )
+        return resultado
+
+
+    def listar_libros2(self, kword, fecha1, fecha2):
+
+        date1 = datetime.datetime.strptime(fecha1, "%Y-%m-%d").date()
+        date2 = datetime.datetime.strptime(fecha2, "%Y-%m-%d").date()
+
+        resultado = self.filter(
+            titulo__icontains=kword,
+            fecha__range=(date1, date2)
         )
         return resultado
--- applications/libro/views.py
+++ applications/libro/views.py
@@ -9,4 +9,10 @@ class ListLibros(ListView):
 
     def get_queryset(self):
         palabra_clave = self.request.GET.get("kword","")
-        return Libro.objects.listar_libros(palabra_clave)
+        f1 = self.request.GET.get("fecha1","")
+        f2 = self.request.GET.get("fecha2","")
+
+        if f1 and f2:
+            return Libro.objects.listar_libros2(palabra_clave, f1, f2)
+        else:
+            return Libro.objects.listar_libros(palabra_clave)
