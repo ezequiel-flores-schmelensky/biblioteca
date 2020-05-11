a6d3117ae8edf333937e72916785634782a81d0f
Ezequiel Flores <supereze.bxb@gmail.com>
2020-05-10 21:25:13 -0500

    [feat] form prestamo

--- applications/lector/views.py
+++ applications/lector/views.py
@@ -1,3 +1,31 @@
+from datetime import date
 from django.shortcuts import render
-
+from django.views.generic.edit import FormView
+from .models import Prestamo
+from .forms import PrestamoForm
 # Create your views here.
+
+class RegistrarPrestamo(FormView):
+    template_name = 'lector/add_prestamo.html'
+    form_class = PrestamoForm
+    success_url = '.'
+
+    def form_valid(self, form):
+
+        # Prestamo.objects.create(
+        #     lector=form.cleaned_data['lector'],
+        #     libro=form.cleaned_data['libro'],
+        #     fecha_prestamo=date.today(),
+        #     devuelto=False
+        # )
+
+        prestamo = Prestamo.objects.create(
+            lector=form.cleaned_data['lector'],
+            libro=form.cleaned_data['libro'],
+            fecha_prestamo=date.today(),
+            devuelto=False
+        )
+        prestamo.save()
+
+        return super(RegistrarPrestamo, self).form_valid(form)
+
